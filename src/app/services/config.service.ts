d82578307c36dca618a9dad158a888c66ebfb508
Carol <cvsouza04@gmail.com>
2019-12-14 22:50:50 -0300

    Update services

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+import { IConfig } from '../models/config.interface';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
+  configUrl = `${environment.apiUrl}config.json`;
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 
-  config = [];
-
-  getConfig(){
-    return this.config;
+  getConfig(): Observable<IConfig> {
+    return this._http.get<IConfig>(this.configUrl);
   }
 }
